cea47702cf473429aa912393a0e8f95975622865
nt10k <natannt10k@hotmail.com>
2017-10-25 20:41:01 -0200

    login

--- Site/login.php
+++ Site/login.php
@@ -1,16 +1,35 @@
-
-<html>
-<head>
-</head>
-<body>
 <?php
 include('header.php');
 ?>
-<form action='/login.php'>
-  <input type='text' name='email' placeholder='Email'>
-  <input type='password' name='senha' value='Senha'>
-  <input type='submit' value='Submit'>
-</form>
-
+     <section>
+      <div class="grid">
+        <div class="grid-row">
+          <div class="grid-col col--4 col--middle">
+			<div class="card">
+			  <div class="card-container">
+				<div class="card-title">
+				  <div class="opacity"></div>
+				  <span>Login</span>
+				</div>
+				<form action='/login.php'>
+				<div class="card-input">
+				  <input type='text' name='email' placeholder='Email'>
+				  <input type='password' name='senha' placeholder='Senha'>
+				  
+				</div>
+			  </div>
+			  <div class="card-action">
+				<div class="action">
+				  <div class="divider">
+				  </div>
+				  <input class='button button-link' type='submit' value='Entrar'>
+				  </form>
+				</div>
+			  </div>
+			</div>
+          </div>
+        </div>
+      </div>
+</section>
 </body>
 </html>
