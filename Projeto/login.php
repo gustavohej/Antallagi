ebb8e17612826b4038b032eabe2c7432d3dfc403
nt10k <natannt10k@hotmail.com>
2017-10-25 19:45:33 -0200

    Nova estrutura projeto.

---
+++ Projeto/login.php
@@ -0,0 +1,16 @@
+
+<html>
+<head>
+</head>
+<body>
+<?php
+include('header.php');
+?>
+<form action='/login.php'>
+  <input type='text' name='email' placeholder='Email'>
+  <input type='password' name='senha' value='Senha'>
+  <input type='submit' value='Submit'>
+</form>
+
+</body>
+</html>
---
+++ Site/header.php
@@ -0,0 +1,49 @@
+ <html>
+ <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+<header>
+      <nav>
+        <div class="logo">
+          <a href="#">Antallagi</a>
+        </div>
+        <div class="search">
+          <form class="" action="index.html" method="post">
+            <div class="">
+              <label for=""></label>
+              <input type="text" name="" value="" placeholder="Pesquisar">
+              <button type="button" name="button"><i class="material-icons">search</i></button>
+            </div>
+          </form>
+        </div>
+        <div class="">
+          <a href="#">Entrar</a>
+        </div>
+        <!--
+        <div class="">
+          <ul>
+            <li>
+              <a href="#"><i class="material-icons">notifications</i></a>
+            </li>
+            <li>
+              <a href="#">
+                <span>
+                  <img src="http://via.placeholder.com/32x32">
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="#"><i class="material-icons">shopping_cart</i></a>
+            </li>
+          </ul>
+        </div>-->
+      </nav>
+    </header>
+	</body>
+	</html>
