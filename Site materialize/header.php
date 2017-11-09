8f05685ecdfd980888180c499c8022118bcd766b
71701192 <71701192@aluno.faculdadecotemig.br>
2017-11-08 22:23:44 -0200

    Adicionados footer.php e header.php

---
+++ Site materialize/header.php
@@ -0,0 +1,53 @@
+ <html>
+ <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">    
+    <link rel="stylesheet" href="css/materialize.css">
+  </head>
+
+  <body>
+<header>
+      <nav>
+        <p class="logo">
+          <a href="#">Antallagi</a>
+        </p>
+        
+          <div class="nav-wrapper">
+            <form>
+              <div class="input-field">
+                <input id="search" type="search" required>
+                <label class="label-icon" for="search"><i class="lHeightSearch material-icons">search</i></label>
+                <i class="material-icons">close</i>
+              </div>
+            </form>
+          </div>
+
+          <div class="entrar">
+            <a href="#">Entrar</a>
+          </div>
+        <!--
+        <div class="">
+          <ul>
+            <li>
+              <a href="#"><i class="material-icons">notifications</i></a>
+            </li>
+            <li>
+              <a href="#">
+                <span>
+                  <img src="http://via.placeholder.com/32x32">
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="#"><i class="material-icons">shopping_cart</i></a>
+            </li>
+          </ul>
+        </div>-->
+      </nav>
+    </header>
+	</body>
+	</html>
